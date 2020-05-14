5b6efee4ce8a945e3132a7aced0d3f7c4477f78c
Yan <yan.i.andrade@hotmail.com>
2020-05-13 21:04:45 -0300

    1º Commit

---
+++ principal.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="Style.css" type="text/css">
+<link rel="stylesheet" href="fontes/font-awesome.min.css">
+
+<script src="https://kit.fontawesome.com/54f9cce8ca.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+
+
+
+</head>
+<body>
+
+<!----------------------------------------Navbar------------------------------------------>
+
+    <header class="header">  
+        <nav class="navbar navbar-default fixed-top">
+            <div class="container">
+                <div class="navbar-header" >
+                    <a href="#" class="navbar-brand" id="logo"> <img src="Imagens/logo-site0000.png" alt=""></a>
+                </div>
+                <ul class="nav navbar-icones">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+<!-------------------------------------Card - Equipe-------------------------------------------------->
+      <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                <div class="mainflip">
+                    <div class="frontside">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <p><img class=" img-fluid" src="Imagens/CARD1.png" alt="card image"></p>
+                                <h4 class="card-title">Nome</h4>
+                                <p class="card-text">Função.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="backside">
+                        <div class="card">
+                            <div class="card-body text-center mt-4">
+                                <h4 class="card-title">Nome</h4>
+                                <p class="card-text">Texto teste - Exemplo = This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                                <ul class="list-inline">
+                                    <li class="list-inline-item">
+                                            <a href="#" class="social-somos" id="teste">
+                                                    <i class="fab fa-facebook" id="i"></i>
+                                            </a>
+                                            <a href="#" class="social-somos" id="teste">
+                                                    <i class="fab fa-instagram" id="i"></i>
+                                            </a>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>          
+<!--------------------------------------------------------------------------------------------------->
+<!---------------------------------------------------------------------------------------->
+<div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+            <div class="mainflip">
+                <div class="frontside">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <p><img class=" img-fluid" src="Imagens/CARD1.png" alt="card image"></p>
+                            <h4 class="card-title">Nome</h4>
+                            <p class="card-text">Função.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="backside">
+                    <div class="card">
+                        <div class="card-body text-center mt-4">
+                            <h4 class="card-title">Nome</h4>
+                            <p class="card-text">Texto teste - Exemplo = This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                        <a href="#" class="social-somos" id="teste">
+                                                <i class="fab fa-facebook" id="i"></i>
+                                        </a>
+                                        <a href="#" class="social-somos" id="teste">
+                                                <i class="fab fa-instagram" id="i"></i>
+                                        </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>          
+<!--------------------------------------------------------------------------------------------------->
+<!---------------------------------------------------------------------------------------->
+<div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+            <div class="mainflip">
+                <div class="frontside">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <p><img class=" img-fluid" src="Imagens/CARD1.png" alt="card image"></p>
+                            <h4 class="card-title">Nome</h4>
+                            <p class="card-text">Função.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="backside">
+                    <div class="card">
+                        <div class="card-body text-center mt-4">
+                            <h4 class="card-title">Nome</h4>
+                            <p class="card-text">Texto teste - Exemplo = This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                        <a href="#" class="social-somos" id="teste">
+                                                <i class="fab fa-facebook" id="i"></i>
+                                        </a>
+                                        <a href="#" class="social-somos" id="teste">
+                                                <i class="fab fa-instagram" id="i"></i>
+                                        </a>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>          
+<!------------------------------------------Fim - Cards---------------------------------------------------->
+            </div>
+        </div>
+    </section>
+
+<!---------------------------------------Footer------------------------------------------------------------>
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="primeiro-footer mx-auto">
+                            <h4><strong>Futsoccer</strong></h4>
+                            <p> Texto Opicional</p>
+                       </div>
+                        <div class="segundo-footer mx-auto">
+                            <h4><strong>Contato</strong></h4>
+                            <p> E-mail:</p>
+                            <p> Telefone:</p>
+                       </div>
+                        <div class="terceiro-footer mx-auto">
+                            <h4><strong>Redes Sociais</strong><h4>
+                            <a href="#" class="social"> 
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                            <a href="#" class="social">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </div>
+                        <div class="quarto-footer mx-auto">
+                            <h4><strong>Links</strong></h4>
+                            <ul>
+                                <li><a href="#">Home</li>
+                                <li><a href="#">Quem Somos</li>
+                                <li><a href="#">Mídia</li>
+                                <li><a href="#">Atletas</li>
+                                <li><a href="#">Contato</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        </footer>
+
+</body>
+</html>
